e86df91d1767116f4e74e621ec64aecbef54ea86
kwangsun <eyeyel@naver.com>
2026-01-17 18:41:54 +0900

    refactor: 테스트 화면이동 링크 추가

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames/bind";
+import Link from "next/link";
 
 import styles from "./page.module.scss";
 
@@ -7,7 +8,7 @@ const cn = classNames.bind(styles);
 export default function Main() {
   return (
     <main className={cn("Main")}>
-      <h1>메인</h1>
+      <Link href="/test">테스트 페이지로 이동</Link>
     </main>
   );
 }
